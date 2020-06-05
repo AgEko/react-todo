3f202f5e8ded421171ca98c8f422568b387a4022
John Kol <Kolja0@protonmail.com>
2020-06-05 17:41:26 -0500

    Redux project start

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {useState} from 'react';
+import './App.css';
+import List from '@material-ui/core/list'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          React Todo App
+        </p>
+      </header>
+
+    
+
+
+    </div>
+  );
+}
+
+export default App;
